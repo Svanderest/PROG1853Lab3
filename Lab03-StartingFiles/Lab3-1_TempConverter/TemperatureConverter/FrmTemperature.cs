02600e5841230d8bf217780d415dda3631d7fe7e
Svanderest <35896486+Svanderest@users.noreply.github.com>
2019-02-06 22:37:04 -0500

    initial commit

---
+++ Lab03-StartingFiles/Lab3-1_TempConverter/TemperatureConverter/FrmTemperature.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Sebastian van der Est
+namespace TemperatureConverter
+{
+    public partial class FrmTemperature : Form
+    {
+        public FrmTemperature()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcFahrenheit_Click(object sender, EventArgs e)
+        {
+            Double celcius = Convert.ToDouble(txtTemperature.Text);
+            Double farenheit = 9 * celcius / 5 + 32;
+            lblFahrenheit.Text = farenheit.ToString("F2");
+        }
+
+        private void btnCalcCelsius_Click(object sender, EventArgs e)
+        {
+            Double farenheit = Convert.ToDouble(txtTemperature.Text);
+            Double celcius = 5 * (farenheit - 32) / 9;
+            lblCelsius.Text = celcius.ToString("F2");
+        }
+    }
+}
---
+++ Lab03-StartingFiles/Lab3-2_ElectricalCurrent/ElectricalCurrent/FrmElectricalCurrent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Sebastian van der Est
+namespace ElectricalCurrent
+{
+    public partial class FrmElectricalCurrent : Form
+    {
+        public FrmElectricalCurrent()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Double voltage = Convert.ToDouble(txtBatteryVoltage.Text);
+            Double resistance = Convert.ToDouble(txtResistance.Text);
+            Double capacitance = Convert.ToDouble(txtCapacitance.Text);
+            Double time = Convert.ToDouble(txtTime.Text);
+            Double current = voltage * Math.Pow(Math.E, -time / (resistance * capacitance)) / resistance;
+            lblCurrent.Text = current.ToString("E2");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBatteryVoltage.Text = "";
+            txtCapacitance.Text = "";
+            txtResistance.Text = "";
+            txtTime.Text = "";
+            lblCurrent.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
